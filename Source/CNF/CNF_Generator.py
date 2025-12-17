b3bae621ad22e312cd14cf8daaf5ea077cb93c8c
77x17 <phanngung77x17@gmail.com>
2025-12-17 22:43:43 +0700

    [Fix] - CNF_Generator

--- Source/CNF/CNF_Generator.py
+++ Source/CNF/CNF_Generator.py
@@ -56,6 +56,9 @@ class CNFGenerator:
                     related.append((k, v))
             self.encode_sum_equals(related, need)
 
+        # Add condition to ensure bridges don't cross each other
+        self.add_no_crossing_constraints()
+
         return self.cnf, self.var_map
 
     def encode_sum_equals(self, vars_k, total):
@@ -67,3 +70,53 @@ class CNFGenerator:
                 for i,val in enumerate(comb):
                     clause.append(-vars_k[i][1] if val else vars_k[i][1])
                 self.cnf.append(clause)
+
+    def orientation(self, a, b):
+        if a[0] == b[0]:
+            return "H"
+        if a[1] == b[1]:
+            return "V"
+        return None
+
+    def bridge_cells(self, a, b):
+        r1, c1 = a
+        r2, c2 = b
+        cells = set()
+
+        if r1 == r2:  # horizontal
+            for c in range(min(c1, c2) + 1, max(c1, c2)):
+                cells.add((r1, c))
+        elif c1 == c2:  # vertical
+            for r in range(min(r1, r2) + 1, max(r1, r2)):
+                cells.add((r, c1))
+
+        return cells
+    
+
+    def add_no_crossing_constraints(self):
+        bridges = []
+
+        # collect metadata
+        for (a, b, k), v in self.bridge_vars.items():
+            if k == 0:
+                continue
+            bridges.append((
+                v,
+                self.orientation(a, b),
+                self.bridge_cells(a, b)
+            ))
+
+        # forbid H-V crossings
+        for i in range(len(bridges)):
+            v1, o1, cells1 = bridges[i]
+            for j in range(i + 1, len(bridges)):
+                v2, o2, cells2 = bridges[j]
+
+                # only horizontal vs vertical
+                if o1 == o2:
+                    continue
+
+                # real crossing
+                if cells1 & cells2:
+                    self.cnf.append([-v1, -v2])
+
